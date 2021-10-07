177e3547b434c6f940a88342c5ad453cbe4e513c
dimlat <87132012+dimlat@users.noreply.github.com>
2021-10-07 18:39:55 +0700

    add cloud firestore

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,14 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 import 'core/utils/auth_checker.dart';
 import 'features/bottom_nav_bar/presentation/controllers/bottom_nav_bar_controller.dart';
-
+import 'features/home/presentation/controllers/ten_last_data_controller.dart';
 
 //! global variable
 FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
 
+TenLastDataController tenLastDataController = TenLastDataController.instance;
 BottomNavBarController bottomNavBarController = BottomNavBarController.instance;
 AuthCheckerController authCheckerController = AuthCheckerController.instance;
 
@@ -16,6 +19,7 @@ Future<void> init() async {
   await Firebase.initializeApp();
 
   //! controller init
+  Get.put(TenLastDataController());
   Get.put(BottomNavBarController());
   Get.put(AuthCheckerController());
 }
