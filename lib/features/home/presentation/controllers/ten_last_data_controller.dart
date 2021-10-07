b0d44bc46caa8d9de2f6776f43084b325122c7e4
dimlat <87132012+dimlat@users.noreply.github.com>
2021-10-07 18:41:36 +0700

    controller to data

---
+++ lib/features/home/presentation/controllers/ten_last_data_controller.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
+
+import '../../../../injection_container.dart';
+import '../../../prospect/domain/entities/prospect.dart';
+import '../../domain/entities/page_hook.dart';
+
+class TenLastDataController extends GetxController {
+  static TenLastDataController instance = Get.find();
+
+  Future route(PageHook hook) async {
+    switch (hook.section) {
+      case SectionHook.latest:
+        return await _dataLatest(hook.stage);
+      case SectionHook.history:
+        return await _dataHistory();
+      case SectionHook.read:
+        return await _dataRead(hook.stage);
+      default:
+        return [];
+    }
+  }
+
+  Future<List<Prospect>> _dataRead(String stage) async {
+    List<Prospect> list = [];
+
+    try {
+      final data = await firebaseFirestore
+          .collection(Prospect.modelName)
+          .where('stageHook', isEqualTo: stage)
+          .orderBy('createdAt', descending: true)
+          .limit(10)
+          .get();
+      if (data.size > 0) {
+        for (var element in data.docs) {
+          list.add(Prospect.fromMap(element.data()));
+        }
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+
+    return list;
+  }
+
+  Future<List<Prospect>> _dataHistory() async {
+    List<Prospect> list = [];
+
+    try {
+      final data = await firebaseFirestore
+          .collection("DataLog")
+          .orderBy('createdAt', descending: true)
+          .limit(10)
+          .get();
+      if (data.size > 0) {
+        for (var element in data.docs) {
+          list.add(Prospect.fromMap(element.data()));
+        }
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+
+    return list;
+  }
+
+  Future<List<Prospect>> _dataLatest(String stage) async {
+    List<Prospect> list = [];
+
+    try {
+      final data = await firebaseFirestore
+          .collection(Prospect.modelName)
+          .where('stageHook', isEqualTo: stage)
+          .orderBy('createdAt', descending: false)
+          .limit(10)
+          .get();
+
+      if (data.size > 0) {
+        for (var element in data.docs) {
+          list.add(Prospect.fromMap(element.data()));
+        }
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+
+    return list;
+  }
+}
