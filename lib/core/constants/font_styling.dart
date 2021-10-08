7721c931b1b3ef7b3780cc8ff0f1daa0ee80c5e5
dimlat <87132012+dimlat@users.noreply.github.com>
2021-10-08 22:58:44 +0700

    change font style

--- lib/core/constants/font_styling.dart
+++ lib/core/constants/font_styling.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 TextStyle get kFontHeaderStyle => const TextStyle(
-    fontFamily: 'AkayaTelivigala', fontSize: 62, fontWeight: FontWeight.bold);
+    fontFamily: 'AkayaTelivigala', fontSize: 30, fontWeight: FontWeight.bold);
 
 TextStyle get kFontTitleStyle => GoogleFonts.roboto(
-    textStyle: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold));
+    textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.w400));
 
 TextStyle get kFontSubTitleStyle => GoogleFonts.lato(
-    textStyle: const TextStyle(fontSize: 15, fontWeight: FontWeight.w300));
+    textStyle: const TextStyle(fontSize: 12, fontWeight: FontWeight.w300));
 
 
 // this is example if you want to use googlefonts pub
